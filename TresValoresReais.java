778baea1d6d3727449c877d1f78d6bdf75e49562
Lucas Bellio Buldrini <lucasbuldrini@yahoo.com.br>
2023-09-06 01:24:33 -0300

    Indicador de maior valor real JAVA

---
+++ TresValoresReais.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TresValoresReais {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Digite o número A: ");
+        double a = sc.nextDouble();
+        System.out.println("Digite o número B: ");
+        double b = sc.nextDouble();
+        System.out.println("Digite o número C: ");
+        double c = sc.nextDouble();
+        sc.close();
+        if ((a > b) && (a > c)) {
+            System.out.println("O maior valor é A");
+        } else if ((b > a) && (b > c)) {
+            System.out.println("O maior valor é B");
+        } else if ((c > a) && (c > b)); {
+            System.out.println("O maior valor é C");
+        }
+    }
+
+    
+}
