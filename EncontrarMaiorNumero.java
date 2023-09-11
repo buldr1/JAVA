44027ee478ad05c3d5474776ac3a49946453bc79
Lucas Bellio Buldrini <lucasbuldrini@yahoo.com.br>
2023-09-11 13:11:20 -0300

    Maior Número  JAVA

---
+++ EncontrarMaiorNumero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EncontrarMaiorNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        if (numero1 > numero2) {
+            System.out.println(numero1 + " é o maior.");
+        } else if (numero2 > numero1) {
+            System.out.println(numero2 + " é o maior.");
+        } else {
+            System.out.println("Estes números são iguais.");
+        }
+        scanner.close();
+    }
+}
