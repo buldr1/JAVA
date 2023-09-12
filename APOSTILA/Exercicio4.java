2182db86e52dcbb66e299144b4788c869456f854
Lucas Bellio Buldrini <lucasbuldrini@yahoo.com.br>
2023-09-12 04:06:49 -0300

    .

--- APOSTILA/Exercicio4.java
+++ APOSTILA/Exercicio4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EncontrarMaiorNumero {
+public class Exercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- APOSTILA/Exercicio5.java
+++ APOSTILA/Exercicio5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculadoraNumeros {
+public class Exercicio5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
