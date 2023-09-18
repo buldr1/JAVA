daa6cc0774739aba40d063653a34e16c5969d0fe
Lucas Bellio Buldrini <lucasbuldrini@yahoo.com.br>
2023-09-18 20:06:09 -0300

    .

--- APOSTILA 04/EX2.java
+++ APOSTILA 04/EX2.java
@@ -1,5 +1,3 @@
-//Faça um programa que leia um valor N inteiro e positivo, calcule e mostre o valor de E, conforme a fórmula a seguir: E = 1 + 1/1! + 1⁄2! + 1/3! + ... + 1/N!
-
 import java.util.Scanner;
 
 public class EX2 {
--- APOSTILA 04/EX5.java
+++ APOSTILA 04/EX5.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class EX5 {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Quantos termos? ");
+        int quantidade = leitor.nextInt();
+       
+        System.out.println("X? ");
+        double x = leitor.nextDouble();
+        double s = 0;
         
+        for (int expoente = 2; expoente < quantidade + 2; expoente ++) {
+        double divindendo = Math.pow(x, expoente);
+        double divisor = 1;
+        double termo = (divindendo/ divisor);
+            if (expoente % 2 == 0) {
+            s = s + termo;
+            } else {
+            s = s + termo;
+        }
+        }
+        double dividendo = Math.pow(x, 2);
+        double divisor = 1;
+
+        s = - (dividendo / divisor );
+
+        System.out.println(s);
+        leitor.close();
     }
     
 }
