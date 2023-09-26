8d3b34e77ad618fb1960319f4a1d297b00872c93
Lucas Bellio Buldrini <lucasbuldrini@yahoo.com.br>
2023-09-26 19:50:37 -0300

    .

--- CLASS/CompareTO.java
+++ CLASS/CompareTO.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
     public static void main(String[] args) {
     String s1 = JOptionPane.showInputDialog("Qual a primeira string?");
     String s2 = JOptionPane.showInputDialog("Qual a segunda string?");
-//considerando caixa alta e baixa
+//considerando caixa alta e baixa 
     int resultado = s1.compareTo(s2);
     if (resultado < 0) {
     JOptionPane.showMessageDialog(null, s1 + " vem antes de " + s2 + " no dicionário");
